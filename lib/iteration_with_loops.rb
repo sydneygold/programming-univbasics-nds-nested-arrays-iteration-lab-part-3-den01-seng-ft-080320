0a6eb6ee5dd462ca08b0ca9e310d2b120b0ff2b4
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-19 22:39:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ def join_nested_strings(src)
       element_index = 0
       while element_index < src[row_index].count do
         if src[row_index][element_index].class == String
-          p outer_results << src[row_index][element_index]
+          p outer_results << src[row_index][element_index] + " "
           p outer_results
         end
         element_index += 1
@@ -15,5 +15,5 @@ def join_nested_strings(src)
       end
       row_index += 1
     end
-    outer_results.join
+    p outer_results
 end
